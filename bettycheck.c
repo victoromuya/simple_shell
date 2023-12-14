49794d360f5fead715442799d1c5fe9139e8837c
victoromuya <viczik16@gmail.com>
2023-12-14 09:18:32 -0800

    betty check

---
+++ bettycheck.c
@@ -0,0 +1,16 @@
+
+/**
+ * main - Prints addition of two numners
+ *
+ * Return: The addition of two numbers
+ */
+
+
+int main(void)
+{
+	int a = 4;
+	int b = 9;
+	int c = a + b;
+
+	return (c);
+}
